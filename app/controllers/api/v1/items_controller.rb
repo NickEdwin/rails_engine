7b8a22a0057091159a54f9d2280e207bf0ebae2b
Nick Edwin <nickedwin85@gmail.com>
2020-08-30 17:30:44 -0600

    Items controller index and create methods

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::ItemsController < ApplicationController
+
+  def index
+    render json: Item.all
+  end
+
+  def create
+    render json: Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+end
