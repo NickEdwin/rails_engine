4775f8c49987bbaef4283321b1a1eecfc15b8e7e
Nick Edwin <nickedwin85@gmail.com>
2020-08-31 13:50:49 -0600

    Updated merchant serializer to match project requirements

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,8 +1,4 @@
 class MerchantSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name
-
-  has_many :invoices, dependent: :destroy
-  has_many :items, dependent: :destroy
-  has_many :transactions, through: :invoices
+  attributes :name
 end
