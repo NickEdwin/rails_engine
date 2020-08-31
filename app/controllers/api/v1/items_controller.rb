96d5f28bab1ca407a227673d724af50a1fff02ad
Nick Edwin <nickedwin85@gmail.com>
2020-08-31 13:21:21 -0600

    Updated controller for serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,16 +1,22 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    render json: Item.all
+    render json: ItemSerializer.new(Item.all)
   end
 
   def create
-    render json: Item.create(item_params)
+    render json: ItemSerializer.new(Item.create(item_params))
   end
 
   def update
-    item = Item.find(params[:id])
-    render json: item.update(item_params)
+    id = params[:id]
+    render json: ItemSerializer.new(Item.update(id, item_params))
+  end
+
+  def destroy
+    id = params[:id]
+    render json: ItemSerializer.new(Item.find(id))
+    Item.delete(id)
   end
 
   private
