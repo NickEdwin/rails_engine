ed8aaacca4f7098c280fa3820427eb3412e35465
Nick Edwin <nickedwin85@gmail.com>
2020-09-01 13:55:20 -0600

    Route for single merchant show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/merchants/find', to: 'merchants_search#show'
+
       resources :items do
         get '/merchant', to: 'items#merchant_show'
 
