a8ea53e013f4717593256daf7b0732c98308a353
Nick Edwin <nickedwin85@gmail.com>
2020-09-01 14:03:19 -0600

    Changes file near for multi search, created single item return test

--- spec/requests/api/v1/find_spec.rb
+++ spec/requests/api/v1/find_spec.rb
@@ -8,14 +8,26 @@ describe "Find API" do
     create(:merchant, name: "Ring a Ding Ding")
     @merchant1 = Merchant.first
     @merchant2 = Merchant.last
+    create(:item, name: "Banana", merchant: @merchant1)
+    create(:item, name: "Apple", merchant: @merchant1)
+    create(:item, name: "Banana Phone", merchant: @merchant2)
+    @item1 = Item.first
+    @item2 = Item.last
   end
 
   it "can find first merchant matching a name" do
     get '/api/v1/merchants/find?name=ring'
 
-
     merchant = JSON.parse(response.body)
     expect(merchant["data"]["attributes"]["name"]).to eq(@merchant1.name)
     expect(merchant["data"]["attributes"]["name"]).to_not eq(@merchant2.name)
+  end
+
+  it "can find first item matching a name" do
+    get '/api/v1/items/find?name=banana'
+
+    item = JSON.parse(response.body)
+    expect(item["data"]["attributes"]["name"]).to eq(@item1.name)
+    expect(item["data"]["attributes"]["name"]).to_not eq(@item2.name)
   end
 end
