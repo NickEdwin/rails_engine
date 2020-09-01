42e3eefa364e24c217e210d7b12dc7e54baeb399
Nick Edwin <nickedwin85@gmail.com>
2020-09-01 14:03:50 -0600

    Item search controller with method to return first item found

---
+++ app/controllers/api/v1/items_search_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::ItemsSearchController < ApplicationController
+
+  def show
+    item = Item.where("name ilike ?", "%#{item_params[:name]}%").first
+    render json: ItemSerializer.new(item)
+  end
+
+  private
+
+    def item_params
+      params.permit(:name, :description, :created_at, :updated_at)
+    end
+end
