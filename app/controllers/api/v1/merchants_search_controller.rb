d1156673e31e5e567f63320fed13c349c54e588a
Nick Edwin <nickedwin85@gmail.com>
2020-09-01 13:55:57 -0600

    Merchant search controller with single merchant find method

---
+++ app/controllers/api/v1/merchants_search_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::MerchantsSearchController < ApplicationController
+
+  def show
+    merchant = Merchant.where("name ilike ?", "%#{merchant_params[:name]}%").first
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+
+    def merchant_params
+      params.permit(:name, :created_at, :updated_at)
+    end
+end
