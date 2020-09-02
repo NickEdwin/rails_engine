2366002760976f5286557765de9189dc953c2117
Nick Edwin <nickedwin85@gmail.com>
2020-09-02 17:13:58 -0600

    First round of code DRYing

--- app/controllers/api/v1/items_search_controller.rb
+++ app/controllers/api/v1/items_search_controller.rb
@@ -1,47 +1,42 @@
 class Api::V1::ItemsSearchController < ApplicationController
 
   def show
-    if item_params.keys[0] == "name"
-      item = Item.where("name ilike ?", "%#{item_params[:name]}%").first
-    elsif item_params.keys[0] == "description"
-      item = Item.where("description ilike ?", "%#{item_params[:description]}%").first
-    elsif item_params.keys[0] == "unit_price"
-      item = Item.where(unit_price: "#{item_params[:unit_price]}.to_f").first
-    elsif item_params.keys[0] == "created_at"
+    attribute = params.keys.first
+    value = params[attribute]
+
+    if attribute == "name"
+      item = Item.where("name ilike ?", "%#{value}%").first
+    elsif attribute == "description"
+      item = Item.where("description ilike ?", "%#{value}%").first
+    elsif attribute == "unit_price"
+      item = Item.where(unit_price: "#{value}.to_f").first
+    else attribute == "created_at" || "updated_at"
       items = Item.all
       item = items.each do |item|
-        item.created_at.to_date == item_params[:created_at].to_date
+        "item.#{attribute}.to_date" == "#{value.to_date}"
       end
-      item = item[0]
-    else
-      items = Item.all
-      item = items.each do |item|
-        item.updated_at.to_date == item_params[:updated_at].to_date
-      end
-      item = item[0]
+      item = item.first
     end
     render json: ItemSerializer.new(item)
   end
 
   def index
-    if item_params.keys[0] == "name"
-      items = Item.where("name ilike ?", "%#{item_params[:name]}%")
-    elsif item_params.keys[0] == "description"
-      items = Item.where("description ilike ?", "%#{item_params[:description]}%")
-    elsif item_params.keys[0] == "unit_price"
-      items = Item.where(unit_price: "#{item_params[:unit_price]}.to_f")
-    elsif item_params.keys[0] == "created_at"
+    attribute = params.keys.first
+    value = params[attribute]
+
+    if attribute == "name"
+      item = Item.where("name ilike ?", "%#{value}%")
+    elsif attribute == "description"
+      item = Item.where("description ilike ?", "%#{value}%")
+    elsif attribute == "unit_price"
+      item = Item.where(unit_price: "#{value}.to_f")
+    else attribute == "created_at" || "updated_at"
       items = Item.all
-      items = items.each do |item|
-        item.created_at.to_date == item_params[:created_at].to_date
-      end
-    else
-      items = Item.all
-      items = items.each do |item|
-        item.updated_at.to_date == item_params[:updated_at].to_date
+      item = items.each do |item|
+        "item.#{attribute}.to_date" == "#{value.to_date}"
       end
     end
-    render json: ItemSerializer.new(items)
+    render json: ItemSerializer.new(item)
   end
 
   private
--- app/controllers/api/v1/merchants_search_controller.rb
+++ app/controllers/api/v1/merchants_search_controller.rb
@@ -1,36 +1,31 @@
 class Api::V1::MerchantsSearchController < ApplicationController
 
   def show
-    if  merchant_params.keys[0] == "name"
+    attribute = params.keys.first
+    value = params[attribute]
+
+    if  merchant_params.keys.first == "name"
       merchant = Merchant.where("name ilike ?", "%#{merchant_params[:name]}%").first
-    elsif merchant_params.keys[0] == "created_at"
+    else attribute == "created_at" || "updated_at"
       merchants = Merchant.all
       merchant = merchants.each do |merchant|
-        merchant.created_at.to_date == merchant_params[:created_at].to_date
+        "merchant.#{attribute}.to_date" == "#{value.to_date}"
       end
-      merchant = merchant[0]
-    else
-      merchants = Merchant.all
-      merchant = merchants.each do |merchant|
-        merchant.updated_at.to_date == merchant_params[:updated_at].to_date
-      end
-      merchant = merchant[0]
+      merchant = merchant.first
     end
     render json: MerchantSerializer.new(merchant)
   end
 
   def index
-    if  merchant_params.keys[0] == "name"
+    attribute = params.keys.first
+    value = params[attribute]
+
+    if  merchant_params.keys.first == "name"
       merchant = Merchant.where("name ilike ?", "%#{merchant_params[:name]}%")
-    elsif merchant_params.keys[0] == "created_at"
+    else attribute == "created_at" || "updated_at"
       merchants = Merchant.all
       merchant = merchants.each do |merchant|
-        merchant.created_at.to_date == merchant_params[:created_at].to_date
-      end
-    else
-      merchants = Merchant.all
-      merchant = merchants.each do |merchant|
-        merchant.updated_at.to_date == merchant_params[:updated_at].to_date
+        "merchant.#{attribute}.to_date" == "#{value.to_date}"
       end
     end
     render json: MerchantSerializer.new(merchant)
