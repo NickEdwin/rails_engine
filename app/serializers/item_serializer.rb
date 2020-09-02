ec4212b0fc93455e39c124b4c8dccc904b1069e9
Nick Edwin <nickedwin85@gmail.com>
2020-09-02 14:41:07 -0600

    Removed relationships from serializers

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,6 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :unit_price, :merchant_id
+  attributes :name, :description, :unit_price, :merchant_id
 
-  belongs_to :merchant
+  # belongs_to :merchant
 end
